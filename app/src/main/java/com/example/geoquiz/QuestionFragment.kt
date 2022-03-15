d669b3d943b77a206ea6ec6df9a16d6b2e50bbb7
ts moosavi <tt.musavi@gmail.com>
2022-03-15 18:05:19 +0330

    Initial commit

---
+++ app/src/main/java/com/example/geoquiz/QuestionFragment.kt
@@ -0,0 +1,25 @@
+package com.example.geoquiz
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.geoquiz.databinding.FragmentQuestionBinding
+
+class QuestionFragment : Fragment() {
+    lateinit var binding: FragmentQuestionBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentQuestionBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+}
